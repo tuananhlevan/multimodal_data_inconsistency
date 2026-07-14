6306ca09217264c4df339eac5dcd314dd4d88743
levantuananh <tuananhlevan181@gmail.com>
2026-07-14 11:57:45 +0700

    14-07-26 11:57

--- download.sh
+++ download.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
+conda activate data_inconsistency
 python download.py --use_ckpt
--- extract.sh
+++ extract.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
+conda activate data_inconsistency
 python3 /home/levantuananh/FFT_FPT/data_inconsistency/tex_drive_pipeline.py \
   --source-folder-id 1g2uGiWJMCiMenw8_IKFga1M7iIJIaLax \
   --output-folder-id 1PqhB3lAWt5j1lilXg0Mg5ldM2U8M_1_- \
